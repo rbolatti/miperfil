b41aa6d82f5b2e08d1fa1cdeb5f58a9c2220deba
gransamoyedo <renzobolatti@informaticos.com>
2016-12-22 23:38:56 -0300

    Update minote.sh

--- minote.sh
+++ minote.sh
@@ -4,6 +4,7 @@ apt-get update && apt-get upgrade
 echo alias actu='apt-get update && apt-get upgrade'>>~/.bash_aliases
 echo alias insta='apt-get install'>>~/.bash_aliases
 echo alias muestra='hydra -L usuarios.txt -P password.txt -vV'>>~/.bash_aliases
+source ~/.bash_aliases
 apt-get install hydra
 apt-get install nmap 
 apt-get install vim
