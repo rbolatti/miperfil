6994a80308c2e1cb48e22ae65b6e3870ae5eb0c4
gransamoyedo <renzobolatti@informaticos.com>
2016-12-22 23:08:55 -0300

    Update minote.sh

--- minote.sh
+++ minote.sh
@@ -2,7 +2,8 @@
 # -*- ENCODING: UTF-8 -*-
 echo 'alias actu='apt-get update && apt-get upgrade'>>/home/iksop/.bashrc'
 echo'alias insta='apt-get install'>>/home/iksop/.bashrc'
-echo 'alias muestra='hydra -L usuarios.txt -P password.txt -vV'>>/home/iksop/.bashrc'
+echo 'alias muestra='hydra -L usuarios.txt -P password.txt -vV'>>/home/iksop/.bashrc'rueba
+echo'alias prueba='echo 'prueba''>>/home/iksop/.bashrc'
 apt-get install hydra
 apt-get install nmap 
 apt-get install vim
