859715934d9d5186cd999b14f5140b6de43b2f0a
gransamoyedo <renzobolatti@informaticos.com>
2018-10-16 21:39:06 -0300

    Update minote.sh

--- minote.sh
+++ minote.sh
@@ -4,12 +4,12 @@ sed -i "s/deb cdrom:*/#deb cdrom:/g" /etc/apt/sources.list
 apt-get update && apt-get upgrade
 cat alias > ~/.bash_aliases
 source ~/.bash_aliases
-apt-get install hydra
-apt-get install nmap 
-apt-get install vim
-apt-get install sqlmap
-apt-get install guake
-apt-get install 
+apt-get install hydra -y
+apt-get install nmap -y
+apt-get install vim -y
+apt-get install sqlmap -y
+apt-get install guake y
+apt-get install -y
 mkdir /home/programasc
 wget -P/home/programas http://download2.mikrotik.com/routeros/winbox/3.7/winbox.exe
 exit
