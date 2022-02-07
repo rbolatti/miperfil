9636a39cc6a8d991962b37ea3902203102708855
Renzo Bolatti <36801587+rbolatti@users.noreply.github.com>
2022-02-07 12:13:50 -0300

    Update minote.sh
    Se actualizan los software

--- minote.sh
+++ minote.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 # -*- ENCODING: UTF-8 -*-a
+chmod 777 ~/mypc/ -R
 sed -i "s/deb cdrom:*/#deb cdrom:/g" /etc/apt/sources.list
-apt-get update && apt-get upgrade
+apt-get update && apt-get upgrade -y
 cat alias > ~/.bash_aliases
 source ~/.bash_aliases
-apt-get install hydra -y
-apt-get install nmap -y
-apt-get install vim -y
-apt-get install sqlmap -y
-apt-get install guake y
-apt-get install -y
-mkdir /home/programasc
-wget -P/home/programas http://download2.mikrotik.com/routeros/winbox/3.7/winbox.exe
+apt install hydra -y
+apt install nmap -y
+apt install vim -y
+apt install sqlmap -y
+apt install yakuake -y
+apt install wine64 -y
+apt install ssh -y
+#apt-get install virtualbox -y
+apt install mc -y
+apt install shutter -y
+apt install wine Gecko -y
+cd /home/ ; var=$(ls)
+touch /home/$var/usuarios.txt
+touch /home/$var/machete.txt
+touch /home/$var/password.txt
+cat ~/mypc/usuarios.txt >> /home/$var/usuarios.txt
+cat ~/mypc/machete.txt >> /home/$var/machete.txt
+cat ~/mypc/password.txt >> /home/$var/password.txt
+wget -P /home/$var/Escritorio https://mt.lv/winbox64
 exit
